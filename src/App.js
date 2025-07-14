6a20cbc7a2f97d4f6554e3fe1d9d85272a1bb9e0
maltika <maltikabn@gmail.com>
2025-07-14 11:39:26 +0700

    CSS-External
    I use CSS External

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
+import './style.css';
 
 function App() {
-  const divStyle = {
-    color:'red',
-    background:'powderblue',
-    fontSize:'larger',
-    padding:'3px',
-  }
-  return <div style={divStyle}>Hello World</div> 
+  return(
+    <>
+    <h2>
+      DeveloperThai.com
+    </h2>
+    <div className="title">
+        React & React Native
+    </div>
+    </>
+  ); 
 }
 
 export default App;
