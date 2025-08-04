5179a3c04ba4bff245a3d200fba61a8af40578b0
mb <maltikabn@gmail.com>
2025-08-04 09:52:47 +0700

    error in class-components.js
    error in class-components.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from "react";
 import { Header,Content,Footer } from "./func-components";
-import Calendar from "./class-components";
+import Button from "./class-components";
 
 function App() {
   return(
-    <>
-    <Header/>
-    <p><center><Calendar/></center></p>
-    <Content/>
-    <Footer/>
-    </>
+    <p><center><Button/></center></p>
   ); 
 }
 
