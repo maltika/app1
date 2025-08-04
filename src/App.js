1b165fd84a6b239f8febcc4e965cd59a8d2bef40
mb <maltikabn@gmail.com>
2025-08-04 10:55:38 +0700

    CheckEventData
    CheckEventData

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from "react";
 import { Header,Content,Footer } from "./func-components";
 import Button from "./class-components";
 import {Calculator2} from './calculator'
+import {EventData2} from './event-data'
 
 function App() {
   return(
-    <Calculator2/>
+    <EventData2/>
   ); 
 }
 
