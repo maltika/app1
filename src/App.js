fb0938647413a7fb8d56ca615e897148c8abd0a2
mb <maltikabn@gmail.com>
2025-08-13 14:15:26 +0700

    refs-array
    refs-array

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import { Header,Content,Footer } from "./func-components";
 import Button from "./class-components";
 import {Calculator2} from './calculator'
 import {EventData2} from './event-data'
+import RefsArray from './refs-array'
 
 function App() {
   return(
-    <EventData2/>
+    <RefsArray/>
   ); 
 }
 
