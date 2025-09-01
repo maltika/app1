948799452197a882ab1a1c02c38f316eac732f6d
mb <maltikabn@gmail.com>
2025-09-01 10:40:39 +0700

    quiz-app
    quiz-app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React from "react";
-import { userContext } from "./context";
-import Header2 from "./context-header2";
-import Content2 from "./context-content2";
+import { createContext, useState } from "react";
+import Menu from "./component/Menu";
+import Quiz from "./component/Quiz";
+import Score from "./component/Score";
 
+import './App.css'
+
+export const DataContext = createContext()
 function App() {
-  let [user,setUser] = React.useState();
+  const [appState,setAppState] = useState("menu")
+  const [score,setScore] = useState(0)
   return(
-    <userContext.Provider value={[user,setUser]}>
-    <Header2/>
-    <Content2/>
-    </userContext.Provider>
-  ); 
+    <DataContext.Provider value = {{appState,setAppState,score,setScore}}>
+      <div className="App">
+        <h1>Web Development Quiz</h1>
+        {appState === "menu" && <Menu/>}
+        {appState === "quiz" && <Quiz/>}
+        {appState === "score" && <Score/>}
+      </div>
+    </DataContext.Provider>
+    
+  )
 }
 
 export default App;
