c9531202a813c9f6f2a370db24385daa74934cbe
mb <maltikabn@gmail.com>
2025-09-08 10:04:19 +0700

    navlink
    add navlink.cs,router2.js and edit app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,7 @@
-import { createContext, useState } from "react";
-import Menu from "./component/Menu";
-import Quiz from "./component/Quiz";
-import Score from "./component/Score";
 
-import './App.css'
+import React from "react";
+import Router2 from "./router2";
 
-export const DataContext = createContext()
-function App() {
-  const [appState,setAppState] = useState("menu")
-  const [score,setScore] = useState(0)
-  return(
-    <DataContext.Provider value = {{appState,setAppState,score,setScore}}>
-      <div className="App">
-        <h1>Web Development Quiz</h1>
-        {appState === "menu" && <Menu/>}
-        {appState === "quiz" && <Quiz/>}
-        {appState === "score" && <Score/>}
-      </div>
-    </DataContext.Provider>
-    
-  )
+export default function App(){
+  return <Router2/>
 }
-
-export default App;
