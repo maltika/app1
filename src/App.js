82bef6b9adc28591fa093c3cccb818b547916a0c
mb <maltikabn@gmail.com>
2025-09-08 10:48:00 +0700

    router3
    add router 3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 
 import React from "react";
-import Router2 from "./router2";
+// import Router2 from "./router2";
+import Router3 from "./router3";
 
 export default function App(){
-  return <Router2/>
+  return <Router3/>
 }
