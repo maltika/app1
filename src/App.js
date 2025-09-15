b813d5981b501cb69c8ebac210a01d8654883c4e
mb <maltikabn@gmail.com>
2025-09-15 10:20:57 +0700

    Input_Form_txt,psw,txtarea
    Input text , password and textare

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 
 import React from "react";
-// import Router2 from "./router2";
-import Router3 from "./router3";
+import FormText from "./form-text";
 
 export default function App(){
-  return <Router3/>
+  return <FormText/>
 }
